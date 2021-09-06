575a05a44fc2e2795aacd17b150ede804a9b3ea6
evanstech12345 <evanshcode@gmail.com>
2021-09-06 01:44:45 -0400

    adding the styles for the chat system

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,23 @@
+import NavBar from "./Nav"
+import bluelogo from '../blue-logo.png'
+import axios from "axios"
 function Chat() {
+    const message = () => {
+        axios.get('http://localhost:5000/signup', {
+            message: ''
+      })
+    }
     return(
-        <div className="body">
-                
+        <div className="Body">
+            <NavBar />
+            <div className="img-div">
+            <img src={bluelogo}></img>
+            </div>
+            <div className="chat-div-wrapper">
+            <div className="chat-div">
+
+            </div>
+            </div>
         </div>
     )
 }
